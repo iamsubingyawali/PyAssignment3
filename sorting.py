5d0c47e045aa2bfd2b443293651162ed24969b36
iamsubingyawali <subingyawali7@gmail.com>
2020-07-13 09:42:28 +0545

    First Commit

---
+++ sorting.py
@@ -0,0 +1,34 @@
+def bubble_sort(input_list):
+    pass
+
+
+def insertion_sort(input_list):
+    pass
+
+
+def quick_sort(input_list):
+    pass
+
+
+def merge_sort(input_list):
+    pass
+
+
+def linear_search(input_list):
+    pass
+
+
+def binary_search(input_list):
+    pass
+
+
+def interpolation_search(input_list):
+    pass
+
+
+def dijkstras_algorithm(input_list):
+    pass
+
+
+def tower_of_hanoi(dics):
+    pass
